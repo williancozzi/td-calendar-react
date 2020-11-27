9d8f1883cfca2efd8d4175329f2259c8f684f299
williancozzi <williancozzi@gmail.com>
2020-11-26 23:13:29 -0300

    added functions, modal and other adjusts

--- src/App.js
+++ src/App.js
@@ -2,9 +2,47 @@ import './App.css';
 
 function App() {
     const today = new window.Date();
-    let days = []
+    let showModal = false;
+    let days = [];
+
     for (let i = 1; i <= 30; i++) {
-        days.push(<div>{i}</div>)
+        days.push({
+            month: 11,
+            day: i,
+            reminders: [],
+        })
+    }
+
+    const addReminder = (title, date, city, color) => {
+
+        const reminder = {
+            title: title,
+            date: date.toLocaleDateString('pt-BR'),
+            time: date.getTime(),
+            city: city,
+            color: color,
+        }
+
+        const selectedDay = days.filter((d) => d.day === date.getDate() && d.month === date.getMonth() + 1)[0];
+        days[days.indexOf(selectedDay)].reminders.push(reminder);
+
+        console.log(days);
+    }
+
+    const openModal = (e) => {
+        e.preventDefault();
+        showModal = !showModal;
+        console.log(showModal, 'showmodal');
+        const element = document.getElementById('modal');
+
+        setTimeout(() => {
+            if (showModal) {
+                element.classList.add('modal-show')
+            }
+            else {
+                element.classList.remove('modal-show')
+            }
+        }, 300);
     }
 
     return (
@@ -12,10 +50,13 @@ function App() {
             <div className="wrapper">
                 <div className="calendar">
                     <div className="month">
-                        <div>
+                        <div style={styles.monthSize}>
                             <h2 style={styles.h2New}>November</h2>
                             <p id="actual-date">{today.toDateString()}</p>
                         </div>
+                        <button
+                            className="custom-button"
+                            onClick={event => openModal(event)}>New reminder</button>
                     </div>
                     <div className="weekdays">
                         <div>Sun</div>
@@ -26,16 +67,27 @@ function App() {
                         <div>Fri</div>
                         <div>Sat</div>
                     </div>
-                    <div className="days">
-                        {days}
-                        <div style={styles.otherDays}>1</div>
-                        <div style={styles.otherDays}>2</div>
-                        <div style={styles.otherDays}>3</div>
-                        <div style={styles.otherDays}>4</div>
-                        <div style={styles.otherDays}>5</div>
+                    <div className="month-days">
+                        {days.map((d, index) =>
+                            <div key={index} className={today.getDate() === d.day ? 'today day' : 'day'}>
+                                <div className="day-number">
+                                    {d.day}
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
+
+            <div id="modal">
+                <div >
+                    <div className="container">
+                        <a href="#" onClick={event => openModal(event)}>x</a>
+                        <button className="custom-button" onClick={event => addReminder('titulo 1', new Date(2020, 10, 7), 'gama', 'green')}>Add Reminder</button>
+                    </div>
+                </div>
+            </div>
+
         </div >
     );
 }
@@ -47,8 +99,11 @@ const styles = {
     otherDays: {
         color: "#aaaaaae8",
         fontWeight: "300",
-    }
+    },
+    monthSize: {
+        width: "80%",
+        paddingLeft: "120px",
+    },
 };
-
 
 export default App;
